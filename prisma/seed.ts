33d8009f83b1b108aef676baba6a68fa5715e394
Adam Kovar <kovaradam1@gmail.com>
2022-08-20 14:27:04 +0200

    disable seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -25,11 +25,11 @@ async function seed() {
   });
 }
 
-seed()
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+// seed()
+//   .catch((error) => {
+//     console.error(error);
+//     process.exit(1);
+//   })
+//   .finally(async () => {
+//     await prisma.$disconnect();
+//   });
