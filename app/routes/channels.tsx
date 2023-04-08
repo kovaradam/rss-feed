11ae045ff1a8f731463f37cd0aef1cff2b826b03
Adam Kovar <kovaradam1@gmail.com>
2023-04-08 19:15:49 +0200

    fix usermenu styles

--- app/routes/channels.tsx
+++ app/routes/channels.tsx
@@ -393,7 +393,7 @@ function UserMenu(props: { user: ReturnType<typeof useUser> }) {
           )}
         </ul>
       </details>
-      <div className="invisible absolute top-0 -left-0 z-10 h-full w-full  bg-black opacity-30 peer-open:visible sm:invisible"></div>
+      <div className="invisible absolute top-0 -left-0 z-10 h-full w-full  bg-black opacity-30 peer-open:visible sm:peer-open:invisible"></div>
     </>
   );
 }
