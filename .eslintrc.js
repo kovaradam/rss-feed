b04f83be3089afa4963aa6d5fbfcc97b1b850723
Adam Kovar <kovaradam1@gmail.com>
2023-11-19 12:07:32 +0100

    remove vitest

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,8 @@
  */
 module.exports = {
   extends: [
-    "@remix-run/eslint-config",
-    "@remix-run/eslint-config/node",
-    "@remix-run/eslint-config/jest-testing-library",
-    "prettier",
+    '@remix-run/eslint-config',
+    '@remix-run/eslint-config/node',
+    'prettier',
   ],
-  // we're using vitest which has a very similar API to jest
-  // (so the linting plugins work nicely), but it means we have to explicitly
-  // set the jest version.
-  settings: {
-    jest: {
-      version: 27,
-    },
-  },
 };
