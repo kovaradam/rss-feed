463a7a7fc24c24aa64149e3f46325204daca2c03
qtkovarada <kovaradam1@gmail.com>
2025-04-26 13:35:44 +0200

    update copy

--- app/routes/welcome.tsx
+++ app/routes/welcome.tsx
@@ -12,12 +12,16 @@ export default function Welcome() {
     <main className="relative flex h-[200vh] min-h-screen flex-col-reverse justify-center bg-white sm:h-screen sm:flex-row sm:justify-start dark:bg-slate-900 ">
       <section className="flex h-screen flex-col items-center justify-between bg-blue-100 p-[5%] sm:h-full sm:w-1/3 dark:bg-slate-950">
         <div className="flex flex-col gap-4 pb-8">
-          <p className="text-balance text-4xl font-bold text-blue-900 dark:text-slate-400">
-            Keep up with the latest content.
-            <span className="my-8 block underline dark:text-white dark:no-underline">
+          <p className="text-pretty text-4xl font-bold text-blue-900 dark:text-slate-400">
+            Keep up with the latest from the web.
+            <span className="mt-8 block font-normal dark:text-white">
               No algorithms.
             </span>
-            Just the web.
+            <span className="mb-8 block font-normal italic dark:text-white">
+              No noise.
+            </span>
+            Only the the stuff thant interests{" "}
+            <span className="font-normal italic dark:text-white">you</span>.
           </p>
         </div>
         <img
