a569cd9a762c46682c8c314dda2f8bd76d8529cd
kovaradam <kovaradam@users.noreply.github.com>
2025-04-29 20:45:45 +0000

    Prettified Code!

--- app/web-authn.server.ts
+++ app/web-authn.server.ts
@@ -44,9 +44,7 @@ export class WebAuthnService {
       const authenticationOptions = await generateAuthenticationOptions({
         rpID: this.#relyingPartyId,
 
-        allowCredentials: (
-          await getPasskeysByUser(email)
-        ).map((passkey) => ({
+        allowCredentials: (await getPasskeysByUser(email)).map((passkey) => ({
           id: passkey.credentialId,
           transports: passkey.transports,
         })),
